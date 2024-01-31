34098f527eb3e274ad7ae936a6403d8c5a75d806
Enzo Gusmao <enzogusmaobezerra@outlook.com>
2024-01-31 17:20:01 -0300

    fineshed settings window

--- main.gd
+++ main.gd
@@ -50,3 +50,25 @@ func _on_credits_button_pressed():
 
 func _on_display_option_button_item_selected(index):
 	set_video.set_display(set_video.WINDOW_DISPLAY_ARRAY[index])
+
+
+func _on_resolution_option_button_item_selected(index):
+	set_video.set_resolution(set_video.WINDOW_RESOLUTION_DIC.values()[index])
+
+
+func _on_master_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(0, value)
+	print(AudioServer.get_bus_name(0))
+	print(AudioServer.get_bus_volume_db(0))
+
+
+func _on_music_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, value)
+	print(AudioServer.get_bus_name(1))
+	print(AudioServer.get_bus_volume_db(1))
+
+
+func _on_sound_fxh_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(2, value)
+	print(AudioServer.get_bus_name(2))
+	print(AudioServer.get_bus_volume_db(2))
